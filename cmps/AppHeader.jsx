898cf43c1ffabb96d76e2b6b10c90a406b321a2e
Yosef Berman <yosefberman21@gmail.com>
2023-09-07 23:46:22 +0300

    mail - responsive layout start

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -7,7 +7,7 @@ export function AppHeader() {
 
     useEffect(() => {
         setVisibility(visibility)
-        // console.log(visibility);
+        console.log(visibility);
     }, [visibility])
 
     function isVisible() {
@@ -19,19 +19,19 @@ export function AppHeader() {
             <h3>LOGO!</h3>
         </Link>
 
-        <button className="nav-btn" onClick={() => isVisible()}><i className="fa-solid fa-bars"></i> </button>
+        <button className="nav-btn" onClick={() => isVisible()}><i class="fa-solid fa-bars"></i> </button>
 
         {/* <button className="nav-btn" onClick={() => isVisible()}>
-            <i className="fa-solid fa-ellipsis-vertical"></i>
-            <i className="fa-solid fa-ellipsis-vertical"></i>
-            <i className="fa-solid fa-ellipsis-vertical"></i>
+            <i class="fa-solid fa-ellipsis-vertical"></i>
+            <i class="fa-solid fa-ellipsis-vertical"></i>
+            <i class="fa-solid fa-ellipsis-vertical"></i>
         </button> */}
 
         {visibility && <nav className="main-nav">
-            <NavLink to="/"><i className="fa-solid fa-house"></i></NavLink>
-            <NavLink to="/about"><i className="fa-solid fa-info"></i></NavLink>
-            <NavLink to="/mail"><i className="fa-solid fa-envelope"></i></NavLink>
-            <NavLink to="/note"><i className="fa-solid fa-file"></i></NavLink>
+            <NavLink to="/"><i class="fa-solid fa-house"></i></NavLink>
+            <NavLink to="/about"><i class="fa-solid fa-info"></i></NavLink>
+            <NavLink to="/mail"><i class="fa-solid fa-envelope"></i></NavLink>
+            <NavLink to="/note"><i class="fa-solid fa-file"></i></NavLink>
         </nav>}
     </header>
 }
